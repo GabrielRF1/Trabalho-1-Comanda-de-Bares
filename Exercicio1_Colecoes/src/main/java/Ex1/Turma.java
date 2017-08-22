3188f8bf10759b745fa7cd44b17b63b86986226f
Gabriel <Gabriel@192.168.25.5>
2017-08-22 20:08:00 -0300

    eee

--- Exercicio1_Colecoes/src/main/java/Ex1/Turma.java
+++ Exercicio1_Colecoes/src/main/java/Ex1/Turma.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.exercicio1_colecoes;
+package Ex1;
 
 import java.util.LinkedList;
 
