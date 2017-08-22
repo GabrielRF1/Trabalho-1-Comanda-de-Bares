e3825cd486bac96045c41fa649edca7214336874
Gabriel <Gabriel@192.168.25.5>
2017-08-22 20:07:30 -0300

    eee

---
+++ Exercicio1_Colecoes/src/main/java/com/mycompany/exercicio1_colecoes/Aluno.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exercicio1_colecoes;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Aluno {
+    protected int num_aluno, serie;
+
+    public Aluno(int num_aluno, int serie) {
+        this.num_aluno = num_aluno;
+        this.serie = serie;
+    }
+
+    public int getNum_aluno() {
+        return num_aluno;
+    }
+
+    public int getSerie() {
+        return serie;
+    }
+
+    public void setNum_aluno(int num_aluno) {
+        this.num_aluno = num_aluno;
+    }
+
+    public void setSerie(int serie) {
+        this.serie = serie;
+    }
+    
+    
+}
---
+++ Exercicio1_Colecoes/src/main/java/com/mycompany/exercicio1_colecoes/Turma.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.exercicio1_colecoes;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Turma {
+
+    public static void main(String[] args) {
+        LinkedList<Aluno> alunos;
+        alunos = new LinkedList<>();
+        int j = 1;
+        for (int i = 1; i <= 200;) {
+            for (; i <= 50 * j; i++) {
+                Aluno e = new Aluno(i, j);
+                alunos.addLast(e);
+            }
+            j++;
+        }
+        System.out.println("Número \t\t\t\t\t Série");
+        for(Aluno c: alunos){
+        System.out.printf("%d \t\t\t\t\t %d \n",c.getNum_aluno(),c.getSerie());
+        }
+    }
+}
